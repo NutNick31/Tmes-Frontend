e9a2568d32ad2cd8f51b491457c29fd1c3284003
Sahil <sahillede940@gmail.com>
2022-10-12 18:55:19 +0530

    old nav deleted

--- src/Components/Navbar/NavOpts.js
+++
@@ -1,7 +0,0 @@
-const NavOpts = [
-  { opt: "Home", data: "home", href: "#home" },
-  { opt: "Events", data: "Events", href: "#Events" },
-  { opt: "About", data: "About", href: "#About" },
-];
-
-export default NavOpts;
