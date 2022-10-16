3a9dc9c7e788942349d76a27d7b759eff3dc27d4
Vshal007 <vshalpatel018@gmail.com>
2022-10-16 22:28:58 +0530

    Online events updated

--- src/Pages/Events/Online_Events/Mine_Shot.jsx
+++ src/Pages/Events/Online_Events/Mine_Shot.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Mine_Shot = () => {
-  return <div>Mine_Shot</div>;
+  return <div className="quiz_container">
+    <h1>Mineshot</h1>
+    <p>Upload your photos</p>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfZcfxU8dlkV1PECCCvhFCEGLZ8BKWuT5Y0SpWxURsvSp_UxQ/viewform?usp=sf_link" target="_blank">
+      <button class="btn">Upload</button>
+    </a>
+
+  </div>;
 };
 
 export default Mine_Shot;
