59e26c64f39da9766717cf0ea9673a520569bade
Nutnick <rajesh211019study@gmail.com>
2022-12-05 12:24:57 +0530

    Navbar Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Footer, Navbar, Loader } from "./Components";
+import { Footer, Loader } from "./Components";
+import Navbar from './Components/Navbar/Navbar1'
 import Routing from "./Routing";
 import "./Pages/Events/Offline_Events/Offline_Events.scss";
 import { AnimatePresence } from "framer-motion";
 import AnimatedCursor from "react-animated-cursor";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.scss";
 
 function App() {
@@ -17,21 +19,21 @@ function App() {
 
   return (
     <div className="App container-fluid p-0">
-      <AnimatedCursor
+      {/* <AnimatedCursor
         color="8, 164, 167"
         innerSize={12}
         style={{ zIndex: "1000" }}
-      />
-      {loading && <Loader />}
-      {!loading && (
-        <>
-          <AnimatePresence key="s">
-            <Navbar />
-            <Routing />
-            <Footer />
-          </AnimatePresence>
-        </>
-      )}
+      /> */}
+      {/* {loading && <Loader />} */}
+      {/* {!loading && ( */}
+      <>
+        <AnimatePresence>
+          <Navbar />
+          <Routing />
+          <Footer />
+        </AnimatePresence>
+      </>
+      {/* )} */}
     </div>
   );
 }
